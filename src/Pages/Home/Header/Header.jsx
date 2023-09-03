265b377dff94d82c82392fe9c78aff521194b598
practicewithbyron <practicewithbyron@gmail.com>
2023-09-03 16:50:45 +0100

    Added Home content (without API calls))

---
+++ src/Pages/Home/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+import "../../../App.css";
+import video from "../../../vids/testvid.mp4"
+
+export const Header = () => {
+    return(
+        <div className="header-page flex-row">
+            <div className="flex-column headerText-container">
+                <h1 className="header-title headerTitle-container">Become the developer, <br/> that companies want</h1>
+                <h2 className="header-subtitle">Save countless hours searching for content, <br/> we've got it all here in one place.</h2>
+            </div>
+            <video className="header-vid" controls>
+                <source src={video} type="video/mp4"/>
+            </video>
+        </div>            
+    )
+}
