bfa663e8d8350f88281513f85a6670c94c831b76
practicewithbyron <practicewithbyron@gmail.com>
2023-09-04 09:09:31 +0100

    Added page to test url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './Pages/Home/Home';
+import { TestPage } from './Pages/TestPage';
 
 class App extends Component {
   render()
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <Routes>
         <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/test" element={<TestPage/>}></Route>
       </Routes>
     )
   }
---
+++ src/Pages/TestPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const TestPage = () => {
+    return (
+        <h1>New test page</h1>
+    )
+}
