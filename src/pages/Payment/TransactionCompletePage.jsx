2acc37afdbeb825be5026c3e1698ec4fa63d6213
practicewithbyron <practicewithbyron@gmail.com>
2023-10-26 11:11:38 +0100

    PRAC-90 Fixed eslint error

--- src/pages/Payment/TransactionCompletePage.jsx
+++ src/pages/Payment/TransactionCompletePage.jsx
@@ -7,7 +7,7 @@ import { Button } from './../../components/Button/Button';
 export const TransactionCompletePage = () => {
     return (
         <div className="transactionComplete-container complete-center">
-            <img className="transactionComplete-img" src={require("./check.gif")}/>
+            <img className="transactionComplete-img" src={require("./check.gif")} alt="✅"/>
             <h1 className="transactionComplete-text transactionComplete-title">Thank You for Your Purchase!</h1>
             <h4 className="transactionComplete-text transactionComplete-paragraph">Your order was completed successfully</h4>
             <Button text="Dashboard" func={() => {
