4cd962cd14bc490217f6319701806f9434891224
practicewithbyron <practicewithbyron@gmail.com>
2023-10-29 17:12:24 +0000

    Trying to create an admin page
    Giving annoying react errors so trying a different way.

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Loading } from "../Loading/loading";
 import { Button } from "../../components/Button/Button";
 import { UpdateUserCatalog } from "../../db/Update/updateUserCatalog";
@@ -6,6 +6,9 @@ import { Login } from './../../db/login';
 
 export const AdminPage = () => {
     const [loading, setLoading] = useState(false);
+    const [jwtToken, setJWTToken] = useState("");
+    const [error, setError] = useState("No errors");
+    const [response, setResponse] = useState("");
 
     if(loading)
     {
@@ -16,17 +19,45 @@ export const AdminPage = () => {
     else
     {
         return (
-            <div className="flex-column">
+            <div className="flex-column" style={{marginTop: "50px"}}>
                 <div className="flex-row">
                     <input id="username" type="text" placeholder="username"/>
                     <input id="password" type="text" placeholder="password"/>
-                    <Button text="Login" func={() => {
-                        Login("a2@a.com", "Password1234.")
-                    }}/>
+                    <div style={{maxWidth: "400px"}}>
+                        <Button text="Login" func={() => {
+                            setLoading(true);
+                            Login(`${document.getElementById("username").text}`, `${document.getElementById("password").text}`)
+                            .then(res => {
+                                setJWTToken(res.data.detail.jwt);
+                            })
+                            .catch(err => {
+                                setError(err);
+                            })
+                            .finally(() => {
+                                setLoading(false);
+                            })
+                        }}/>
+                    </div>
+                    <div style={{maxWidth: "400px"}}>
+                        <Button text="Copy" func={() => {
+                            navigator.clipboard.writeText(jwtToken);
+                        }}/>
+                    </div>
+
                 </div>
-                <Button text="Update User Catalog" func={() => {
-                    UpdateUserCatalog(Cookies.)
-                }}/>
+                <div className="flex-row">
+                    <input id="jwt" type="text" placeholder="jwt"/> 
+                    <Button text="Update User Catalog" func={() => {
+                        UpdateUserCatalog(document.getElementById("jwt").text)
+                        .then(res => {
+                            setResponse(res);
+                        })
+                        .catch(err => {
+                            setError(err);
+                        })
+                    }}/>   
+                </div>
+                <h1>{error}</h1>
             </div>
         )
     }
