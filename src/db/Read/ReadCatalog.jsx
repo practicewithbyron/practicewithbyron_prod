96bdb30ebdb2f256bab8534195d9b996ddd0c4cb
practicewithbyron <practicewithbyron@gmail.com>
2023-11-14 21:14:55 +0000

    Updated ReadCatalog API Request.
    Updated ReadCatalog API request to include subscription key and jwt.

--- src/db/Read/ReadCatalog.jsx
+++ src/db/Read/ReadCatalog.jsx
@@ -1,10 +1,19 @@
-import { Post } from '../post';
 import { getURL } from "../getURL";
+import axios from "axios";
 
-export const ReadCatalog = (catalogName) => {
-    return(
-        Post(getURL() + "readCatalog", {
-            catalogName: catalogName
+export const ReadCatalog = (catalogName, jwt) => {
+    return (
+        axios.post(getURL() + "readCatalog", 
+        {
+            "catalogName": catalogName
+        }, 
+        {
+            headers: {
+                "Authorization": jwt,
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+                "Ocp-Apim-Subscription-Key": "ff1ce5d1c42047a3b1f01aeea1e5cfd7"
+            },
         })
     )
 }
