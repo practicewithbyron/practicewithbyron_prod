8290c1cbf7f5d2fc7571e58b267b0f16b570ca19
practicewithbyron <practicewithbyron@gmail.com>
2023-11-14 21:15:44 +0000

    PRAC-100 First Commit.
    First attempt trying to prettify the catalog landing page.

--- src/pages/CatalogLandingPage/CatalogLandingPage.jsx
+++ src/pages/CatalogLandingPage/CatalogLandingPage.jsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { ReadCatalog } from "../../db/Read/ReadCatalog";
 import { useParams } from "react-router-dom";
 import { Loading } from "../Loading/loading";
-import { CatalogLandingPageStatItem } from "./CatalogLandingPageStatItem";
 import { Link } from "react-router-dom";
+import { Error } from "../Error/Error";
+import { CourseOverview } from './CourseOverview';
+import { StarRating } from "../../components/StarRating/StarRating";
+import { Button } from './../../components/Button/Button';
 
 import "./CatalogLandingPage.css";
 import "../../App.css";
-import { Error } from "../Error/Error";
+import { CatalogLandingPageQandA } from "./CatalogLandingPageQandA";
+import { difficultyClassName } from "../../components/Widget/difficultyClassName";
+
+
 
 export const CatalogLandingPage = () => {
     const [isFetching, setIsFetching] = useState(true);
@@ -42,38 +48,42 @@ export const CatalogLandingPage = () => {
     }
     else if (data){
         return (
-            <div className="catalogLanding-page center-text">
-                <div className="catalogLandingHead-container">
-                    <h1 className="catalogLanding-title">
-                        {data.name}
-                    </h1>
-                    <h2 className="catalogLanding-subtitle">
-                        {data.shortDescription}
-                    </h2>
-                    <Link to={`/payment/${name}`}>
-                        <div className="catalogLanding-button fit-content complete-center hoverElement">
-                            🛒 Enroll in Course
-                        </div>
-                    </Link>
-
-                </div>
-
-                <div className="catalogLandingContent-container flex-row">
-                    <div className="catalogLandingDesc-container flex-column" >
-                        <h2 className="bold" style={{marginBottom: "25px"}}>
-                            {data.name}: An {data.shortDescription}
+            <div className="flex-column">
+                <div className="catalogLanding-page center-text flex-row">
+                    <div className="catalogLandingHead-container flex-column">
+                        <p className={`widgetComponent-difficulty ${difficultyClassName(data.difficulty)}`}>{data.difficulty}</p>
+                        <h1 className="catalogLanding-title">
+                            {data.name}
+                        </h1>
+                        <h2 className="catalogLanding-subtitle">
+                            {data.shortDescription}
                         </h2>
-                        <p style={{textAlign: "left"}}>
-                            {data.description}
-                        </p>
+                        <div className="catalogLandingPageStarRating-container">
+                            <StarRating num={data.starRating}/>
+                        </div>
+                        <Button text="🛒 Enroll in Course" func={() => {window.location.href = `/payment/${name}`}}/>
                     </div>
-                    <div className="catalogLandingStats-container flex-column">
-                      <CatalogLandingPageStatItem text="Intermediate" img="glyphicon glyphicon-stats"/>  
-                      <CatalogLandingPageStatItem text="Lifetime Access" img="glyphicon glyphicon-bell"/>  
-                      <CatalogLandingPageStatItem text="140 Questions" img="glyphicon glyphicon-question-sign"/>  
+                    <div className="catalogLandingContent-container textAlign-left flex-column">
+                        <CourseOvervisiew difficulty={data.difficulty} noOfQuestions={140}/>
                     </div>
+                </div>
+                <div className="flex-column catalogLandingPageDescription-container">
+                    <h1 className="catalogLandingPageDescription catalogLandingPageDescription-title">Accurate, Original and Extensive</h1>
+                    <h4 className="catalogLandingPageDescription catalogLandingPageDescription-text">
+                        Tired of having to cobble together various practice questions from spureous website which can't be trusted?
+                        I have produced this course as a bite-size way to make sure you have the resources to ace your exam.
+                    </h4>
+                    <video className="catalogLandingPageDescription-video" controls>
+                        <source src={""} type="video/mp4"/>
+                    </video>
+                </div>
+                <div className="flex-column catalogLandingPageQandA-container">
+                    <CatalogLandingPageQandA title={"What you'll recieve"} tip={"Extensive"} array={["✅ 140 original questions that accurately reflect the exam", "✅ Personalised training plan to make help you practice at your own pace"]} icon={"check"} color={"#10c737"}/>
+                    <CatalogLandingPageQandA title={"Who is this for?"} tip={"Target Student"} array={[`- Anyone wishing to pass the ${data.name} exam`, " - Those wishing to boost their career", "- Developers who want to take control of their personal development"]} icon={"user"} color={"#A00FF0"}/>
+                    <CatalogLandingPageQandA title={"Prerequisites"} tip={"What you should know"} array={["Since this is an associate exam we advise that you have completed the fundamental exam, if it's not a requierment already."]} icon={"question-sign"} color={"#e49205"}/>
                 </div>
             </div>
+
         )
     }
 }
--- src/pages/CatalogLandingPage/CatalogLandingPageStatItem.jsx
+++ src/pages/CatalogLandingPage/CatalogLandingPageStatItem.jsx
@@ -6,7 +6,7 @@ export const CatalogLandingPageStatItem = ({img, text}) => {
     return(
         <div className="flex-row catalogLandingPageStatItem-container">
             <div className={`${img} catalogLandingPageStatItem-icon`}/>
-            <h3><b>{text}</b></h3>
+            <h3 className="catalogLandingPageStatItem-text"><b>{text}</b></h3>
         </div>
     )
 }
