6d51e4293cdf94215a1949300703bfe943ffd654
practicewithbyron <practicewithbyron@gmail.com>
2023-11-14 21:13:48 +0000

    Abstracted Widget Component.
    Abstracted function for working out the appropriate class name.
    Removed CSS classes from WidgetComponent.css
    Added star rating and difficulty to catalog landing page.

--- src/components/Widget/WidgetComponent.jsx
+++ src/components/Widget/WidgetComponent.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import "../../App.css";
 import "./WidgetComponent.css";
 import { StarRating } from "../StarRating/StarRating";
+import { difficultyClassName } from "./difficultyClassName";
 
-export const WidgetComponent = ({img, text, desc}) => {
+export const WidgetComponent = ({img, text, desc, price, difficulty, starRating}) => {
     return (
         <div className="flex-column widget-component">
             <div className="widgetComponentImage-container">
                 <img src={require(`../../imgs/${img}`)} className="widgetComponent-image" alt={img}></img>
             </div>
             <div className="widgetComponentText-container">
-                <p className="widgetComponent-difficulty widgetComponentDifficulty-beginner">Beginner</p>
+                <p className={`widgetComponent-difficulty ${difficultyClassName(difficulty)}`}>{difficulty}</p>
                 <h3 className="widgetComponent-header">{ text }</h3>
                 <p className="widgetComponent-desc center-text">{ desc }</p>
                 <div className="flex-row" style={{marginTop: "15px"}}>
                     <h1 className="widgetComponent-header">
                         £14.99
                     </h1>
-                    <StarRating num={3.1}/>
+                    <div className="widgetComponentStarRating-container">
+                        <StarRating num={starRating}/>
+                    </div>
                 </div>
             </div>
         </div>
