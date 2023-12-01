f8114a2b75834a9d3a61d7aa743d76cbf1c168f8
practicewithbyron <practicewithbyron@gmail.com>
2023-12-01 15:49:09 +0000

    Updated front-end to include subscription header.
    General cleanup, deleted unused functions.

--- src/db/Read/ReadCatalog.jsx
+++ src/db/Read/ReadCatalog.jsx
@@ -3,17 +3,8 @@ import axios from "axios";
 
 export const ReadCatalog = (catalogName, jwt) => {
     return (
-        axios.post(getURL() + "readCatalog", 
-        {
+        Post(getURL() + "readCatalog", {
             "catalogName": catalogName
-        }, 
-        {
-            headers: {
-                "Authorization": jwt,
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*",
-                "Ocp-Apim-Subscription-Key": "ff1ce5d1c42047a3b1f01aeea1e5cfd7"
-            },
-        })
+        }, jwt)
     )
 }
