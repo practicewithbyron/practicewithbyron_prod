498bd9bbb9890c0fad9a6ea0fae6a2f535b5de39
practicewithbyron <practicewithbyron@gmail.com>
2023-12-05 11:18:55 +0000

    Updated HomePage video.

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import "../../../App.css";
-import video from "../../../vids/testvid.mp4"
+import video from "../../../vids/homePage.mp4"
 
 export const Header = () => {
     return(
@@ -10,7 +10,7 @@ export const Header = () => {
                 <h1 className="header-title headerTitle-container">Become the developer, <br/> that companies want</h1>
                 <h2 className="header-subtitle">Save countless hours searching for content, <br/> we've got it all here in one place.</h2>
             </div>
-            <video className="header-vid" controls>
+            <video className="header-vid" controls muted autoPlay>
                 <source src={video} type="video/mp4"/>
             </video>
         </div>            
