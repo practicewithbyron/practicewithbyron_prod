a190355bfa7c68f98bf015cef4d53a393c74708c
practicewithbyron <practicewithbyron@gmail.com>
2024-02-27 09:29:42 +0000

    Create LoginMessage.jsx

---
+++ src/pages/LoginPage/LoginMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const LoginMessage = () => {
+    return (
+        <div className="a1">
+            <div className="b1">
+                <h1>Please login or register to continue</h1>
+            </div>
+        </div>
+    )
+}
