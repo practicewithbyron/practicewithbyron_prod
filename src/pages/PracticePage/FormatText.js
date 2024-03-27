c1b0e507fa118030aad06e693a5c77bbd8876e6f
practicewithbyron <practicewithbyron@gmail.com>
2024-03-27 22:25:11 +0000

    Update FormatText.js

--- src/pages/PracticePage/FormatText.js
+++ src/pages/PracticePage/FormatText.js
@@ -12,14 +12,14 @@ export const formatText = (inputString, language) => {
     // Iterate through each line
     for (const line of lines) {
         var formattedLine = "";
-        for (const char of line){
-            if (char == "`"){
-                if (isCode == true){ //If currently in a code snippet
+        for (const char of line) {
+            if (char == "`") {
+                if (isCode == true) { //If currently in a code snippet
                     isCodeEnd = true;
                 }
                 isCode ^= true;
             }
-            else if (char == "£"){
+            else if (char == "£") {
                 formattedLine += "\t";
             }
             else {
@@ -27,14 +27,14 @@ export const formatText = (inputString, language) => {
             }
             // Remove the initial '*' and replace it with a tab space
         }
-        if (isCode || isCodeEnd){
+        if (isCode || isCodeEnd) {
             elements.push(<SyntaxHighlighter language={language} style={docco} wrapLongLines={true}>
                 {formattedLine}
             </SyntaxHighlighter>)
         }
-        else{
+        else {
             // Add the formatted line to the array
-            elements.push(<p style={{"margin": "10px 0"}}>{formattedLine}</p>);
+            elements.push(<p style={{ "margin": "10px 0" }}>{formattedLine}</p>);
         }
         isCodeEnd = false;
     }
